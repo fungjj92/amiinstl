8ef92243e5f1d79451da9249375dce7e46d46e2d
Clarence Leung <cleung@netflix.com>
2015-08-21 21:53:10 -0700

    Prep for Heroku deployment

--- amiinstl/settings.py
+++ amiinstl/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -75,21 +77,8 @@ WSGI_APPLICATION = 'amiinstl.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'NAME': 'AmIInSTL',
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': 'jfung',
-        'PASSWORD': 'civicdataftw',
-        'HOST': '',
-        'PORT': '',
-    }
-
-   # {
-   #     'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   # }
-}
+DATABASES = {}
+DATABASES['default'] =  dj_database_url.config()
 
 
 # Internationalization
@@ -105,6 +94,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+STATIC_ROOT = 'staticfiles'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
