4ea85c5a3ec9f94a9ab74ad5f7179fd01486c3d4
fungjj92 <fungjj92@gmail.com>
2015-08-23 22:29:03 -0500

    setupPostgreSQL

--- amiinstl/wsgi.py
+++ amiinstl/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import psycopg2
+import urlparse
+
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
@@ -17,3 +20,14 @@ application = Cling(get_wsgi_application())
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "amiinstl.settings")
 
 application = Cling(get_wsgi_application())
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["postgres://rhywvyexvbbocs:Tw_MI1MN5Z2ZM1PpTUdPxo_RH9@ec2-107-21-114-132.compute-1.amazonaws.com:5432/d694503bvgu2ue"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
